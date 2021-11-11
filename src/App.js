96fce16ed1805c1c4c5b1c444dd8610383e18bea
mushfikurr <mushfikur0@gmail.com>
2021-11-11 16:33:03 +0000

    Several fixes to make it run on GAE

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    refreshTime();
+  }, []);
+
+  const refreshTime = () => {
+    fetch("/time")
+      .then((res) => res.json())
+      .then((data) => {
+        setCurrentTime(data.time);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Testing with the backend....
+        <p>The current time is {currentTime}.</p>
+        <button onClick={refreshTime}>Refresh the time!</button>
       </header>
     </div>
   );
